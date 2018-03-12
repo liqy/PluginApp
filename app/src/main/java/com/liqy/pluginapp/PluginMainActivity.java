1406132817c2fcc17cd58f1683e6b3d941fc68d3
李小义 <unsplash@qq.com>
2018-03-12 20:57:38 +0800

    Initial commit

---
+++ app/src/main/java/com/liqy/pluginapp/PluginMainActivity.java
@@ -0,0 +1,17 @@
+package com.liqy.pluginapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 教程
+ * https://www.jianshu.com/p/013510c19391
+ */
+public class PluginMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plugin_activity_main);
+    }
+}
